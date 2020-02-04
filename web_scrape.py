ea086aab6e6d0d37cc8a4104df8fce32a99d518c
Elizabeth Vitale <elizabethavitale@gmail.com>
2020-02-04 16:31:45 -0500

    fixed comments

--- web_scrape.py
+++ web_scrape.py
@@ -23,22 +23,6 @@ if not (path.exists("encoded_data.html")):
 with open('datasheet.csv', 'w', newline='') as csv_file:
     pass
 
-url = 'https://edgehealth.cox.com/index.php?body=listver.php&sysDescr=ARRIS+DOCSIS+3.0+%2F+PacketCable+2.0+Touchstone+Residential+Gateway+%3C%3CHW_REV%3A+11%3B+VENDOR%3A+ARRIS+Group%2C+Inc.%3B+BOOTR%3A+4.2.0.45%3B+SW_REV%3A+10.1.10.SIP.PC20.CT_TG1682COX_3.12p8s1_PROD_sey%3B+MODEL%3A+TG1682G%3E%3E'
-
-#r2 = requests.get(url, auth=(name,password), verify=False, stream=True, timeout=None)
-#for line in r2.iter_lines():
-#    if("<td nowrap align=left id=report><font color=><a href='?body=ping.php&ip_address=".encode() in line):
-#        print(line)
-#        line = line.decode()
-#        ending = line.find("</a")
-#        print(ending)
-#        beginning = line.find("'>")
-#        result = line[beginning+2:ending]
-#        print(beginning+2)
-#        print(result)
-#        break
-
-
 z = open("encoded_data.html", "r")
 soup = BeautifulSoup(z, 'html5lib')
 tablesData = soup.select('table.stickyHeader tr')
